4a943d1253bde29720a7aa63b87368f4cce37de8
Martin Ponce <doc.brown.83@gmail.com>
2018-02-21 20:40:44 +0800

    Simplified mapping letter to score, replaced map with int[].

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,51 +1,26 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 class Scrabble {
-
-    Map<String, Integer> scores = new HashMap() {
-        private static final long serialVersionUID = 1L;
-        {
-            put("", 0);
-            put("A", 1);
-            put("B", 3);
-            put("C", 3);
-            put("D", 2);
-            put("E", 1);
-            put("F", 4);
-            put("G", 2);
-            put("H", 4);
-            put("I", 1);
-            put("J", 8);
-            put("K", 5);
-            put("L", 1);
-            put("M", 3);
-            put("N", 1);
-            put("O", 1);
-            put("P", 3);
-            put("Q", 10);
-            put("R", 1);
-            put("S", 1);
-            put("T", 1);
-            put("U", 1);
-            put("V", 4);
-            put("W", 4);
-            put("X", 8);
-            put("Y", 4);
-            put("Z", 10);
-        }
-    };
 
     String word;
 
+    /**
+     * Solved with map and reduce. Very cool!
+     * see https://community.oracle.com/docs/DOC-916777
+     */
+
+    // Populate this with scores for each letter
+    // ie: scores[0] = A's score, scores[1] = B's score, ..., scores[25] = Z's score
+    int[] scores = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
     Scrabble(String word) {
-        this.word = word;
+        // Set everything to uppercase, avoid having to handle lowercase chars
+        this.word = word.toUpperCase();
     }
 
+    // 'word' is converted to char stream, where each char is mapped to its score, then reduced to a sum.
     int getScore() {
-        return Arrays.stream(word.toUpperCase().split(""))
-            .map(letter -> scores.get(letter))
-            .reduce(0, Integer::sum);
+        return word
+            .chars()
+            .map(letter -> scores[letter - 'A']) // ie: ('A' - 'A' = 0) == (67 - 67 = 0), return scores[0]
+            .sum(); // reduce to sum of scores
     }
 }
