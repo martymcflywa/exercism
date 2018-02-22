69ddd07a52ab1e6aa83a4176c9c29586a5b08395
Martin Ponce <doc.brown.83@gmail.com>
2018-02-22 10:41:43 +0800

    Implemented raindrops, tests green.

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class RaindropConverter {
 
+    Map<Integer, String> factorWords = new HashMap<>() {
+		private static final long serialVersionUID = 1L;
+        {
+            put(3, "Pling");
+            put(5, "Plang");
+            put(7, "Plong");
+        }
+    };
+
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder sb = new StringBuilder();
+
+        for(int factor : getFactors(number))
+            if(factorWords.containsKey(factor))
+                sb.append(factorWords.get(factor));
+
+        if(sb.length() > 0)
+            return sb.toString();
+
+        return String.valueOf(number);
     }
 
+    List<Integer> getFactors(int number) {
+        List<Integer> factors = new ArrayList<>();
+        for(int i = 1; i <= Math.sqrt(number); i++) {
+            if(number % i == 0) {
+                factors.add(i);
+                if(i != number / i)
+                    factors.add(number / i);
+            }
+        }
+        return factors;
+    }
 }
